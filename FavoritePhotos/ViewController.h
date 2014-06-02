f311b999e9189dd3742908e2618857e30522f53a
jojomac <joanne.mcnamee@d214.org>
2014-06-02 15:09:49 -0500

    Initial Commit

---
+++ FavoritePhotos/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FavoritePhotos
+//
+//  Created by Joanne McNamee on 6/2/14.
+//  Copyright (c) 2014 JMWHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FavoritePhotos/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FavoritePhotos
+//
+//  Created by Joanne McNamee on 6/2/14.
+//  Copyright (c) 2014 JMWHS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
